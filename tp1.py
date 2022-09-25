180ba0c8bc0bbc6aa2f8b18a4d71813758bbd16f
Enzo Damion <enzo.damion@free.fr>
2022-09-25 13:07:42 +0200

    improve data generator fonction with split feature

--- tp1.py
+++ tp1.py
@@ -1,19 +1,32 @@
 import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import random_split
+
 
 def genData():
-    x = np.zeros([1000000, 2])
-    y = np.zeros(1000000)
+    # Generate dataset
+    n_data=1000000
+    dataset = np.zeros([n_data, 3])
     cpt = 0
     for u_iter in np.arange(-5,5,0.01):
         for v_iter in  np.arange(-5,5,0.01):
-            x[cpt,0] = u_iter
-            x[cpt,1] = v_iter
-            y[cpt] = np.sin(u_iter-v_iter)
+            dataset[cpt,0] = u_iter
+            dataset[cpt,1] = v_iter
+            dataset[cpt,2] = np.sin(u_iter-v_iter)
             cpt+=1
-    return x,y
+    # Split and shuffle dataset
+    split_percent = [0.7, 0.15, 0.15]
+    split_length = []
+    for percent in split_percent:
+        split_length.append(int(n_data*percent))
+    train_data, val_data, test_data = random_split(dataset, split_length)
+    return train_data, val_data, test_data
+
 
 def main():
-    x,y = genData()
+    train_data, val_data, test_data = genData()
+
 
 if __name__ == "__main__":
     main()

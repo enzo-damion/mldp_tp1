ad5a002d24f416dae297587c6e1a11b87208ad39
Enzo Damion <enzo.damion@free.fr>
2022-09-26 12:07:27 +0200

    add test function and visualisation tools + model trained with 2000epoch (lr=0.05)

--- tp1.py
+++ tp1.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 
 def genData():
@@ -38,15 +39,38 @@ class Net(nn.Module):
         return x
 
 
-def test(model, data):
-    x = torch.tensor(data[:,:-1])
-    y = torch.tensor(data[:,-1])
-    haty = model(x)
-    return 
+def test(model, test_data):
+    x = torch.FloatTensor(test_data[:,:-1])
+    y = torch.FloatTensor(test_data[:,-1])
+    haty = model(x).view(150000)
+    criterion = nn.MSELoss()
+    loss = criterion(haty, y)
+
+    # Prepare plot
+    y = y.detach().numpy()
+    haty = haty.detach().numpy()
+    mpl.rcParams['agg.path.chunksize'] = 10000
+
+    # Plot regression line
+    _, ax = plt.subplots()
+    ax.plot(y, haty, ".b", markersize=0.1)
+    b, a = np.polyfit(y, haty, deg=1)
+    xseq = np.linspace(-1, 1, num=100)
+    ax.plot(xseq, a + b * xseq, color="k")
+    ax.set(xlabel='Gold labels', ylabel='Prediction', title='Regression line')
+    ax.grid()
+    plt.show()
+
+    # Plot 3d graph
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    # ax.plot(x[:5000,0],x[:5000,1],haty[:5000], ".b", markersize=0.5)
+    # plt.show()
+    return loss
 
 
 def train(model, train_data, max_epoch):
-    optim = torch.optim.Adam(model.parameters(), lr=0.01)
+    optim = torch.optim.Adam(model.parameters(), lr=0.05)
     criterion = nn.MSELoss()
     plot_values = {"epoch": [], "loss": []}
 
@@ -62,6 +86,7 @@ def train(model, train_data, max_epoch):
         optim.step()
         plot_values["epoch"].append(epoch)
         plot_values["loss"].append(loss.item())
+        
     # Plot loss evolution
     _, ax = plt.subplots()
     ax.plot(plot_values["epoch"], plot_values["loss"])
@@ -75,10 +100,18 @@ def train(model, train_data, max_epoch):
 def main():
     # Generate data
     train_data, val_data, test_data = genData()
-    # Create model
+
+    # Create and load model
     model = Net(2,1)
-    # Train model
-    train(model, train_data, 100)
+    model.load_state_dict(torch.load('./model2.pth'))
+
+    # Train and save model
+    # train(model, train_data, 2000)
+    # torch.save(model.state_dict(), "./model2.pth")
+
+    # Test model
+    loss = test(model, test_data)
+    print(f"test loss = {loss}")
 
 
 if __name__ == "__main__":
